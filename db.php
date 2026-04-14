c061e28f4b8bb1e54438006c967c214ac4f59785
tiagodacunha2203-byte <tiagodacunha2203@gmail.com>
2026-04-14 08:57:04 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,11 @@
+<?php
+require_once('credentials.php');
+
 try {
     $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (Exception $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
+?>
